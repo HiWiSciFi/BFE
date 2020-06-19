13c69b6b85775ec58c12f872464c2fac83f13dd4
HiWiSciFi <max.j.hager@gmail.com>
2020-06-19 11:54:38 +0200

    Update IniLoader.java

--- BFE/src/bfeditor/io/IniLoader.java
+++ BFE/src/bfeditor/io/IniLoader.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import bfeditor.logic.Main;
+
 /*
  * .ini system
  * 
@@ -24,15 +26,34 @@ public class IniLoader {
 	private String path;
 	private String[] fileContents;
 	
+	private ArrayList<ArrayList<String>> valuesToSave = new ArrayList<ArrayList<String>>();
+	
 	public IniLoader(String path) throws IOException {
 		this.path = path;
 		FileReader fileReader = new FileReader(path);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         List<String> lines = new ArrayList<String>();
         String line = null;
+        String currPl = Main.EMPTY_STRING;
+        int currPlID = 0;
+        valuesToSave.clear();
         while ((line = bufferedReader.readLine()) != null) {
             lines.add(line);
-            System.out.println(line);
+            if (line.startsWith("[") && line.contains("]")) {
+            	StringBuilder sb = new StringBuilder();
+            	for (int i = 1; i < line.length(); i++) {
+            		if (line.charAt(i) == ']') {
+            			break;
+            		} else {
+            			sb.append(line.charAt(i));
+            		}
+            	}
+            	currPl = sb.toString();
+            	valuesToSave.add(new ArrayList<String>() {private static final long serialVersionUID = 1L;{add(sb.toString());}});
+            	currPlID = valuesToSave.size()-1;
+            } else {
+            	valuesToSave.get(currPlID).add(line);
+            }
         }
         bufferedReader.close();
         fileContents = lines.toArray(new String[lines.size()]);
@@ -71,7 +92,7 @@ public class IniLoader {
 				currPlugin = sb.toString();
 				System.out.println(currPlugin);
 			}
-			if (fileContents[s].startsWith(varname) && currPlugin.equals(plugin)) {
+			if (fileContents[s].startsWith(varname + " = ") && currPlugin.equals(plugin)) {
 				System.out.println("found var");
 				for (int i = 0; i < fileContents[s].length(); i++) {
 					if (fileContents[s].charAt(i) == 34) { // ascii: 34 = "
@@ -91,8 +112,6 @@ public class IniLoader {
 		return null;
 	}
 	
-	ArrayList<ArrayList<String>> valuesToSave = new ArrayList<ArrayList<String>>();
-	
 	public void Save(String plugin, String varname, String value) {
 		int pluginID = -1;
 		for (int i = 0; i < valuesToSave.size(); i++) {
@@ -104,15 +123,28 @@ public class IniLoader {
 			valuesToSave.add(new ArrayList<String>() {private static final long serialVersionUID = 1L;{add(plugin);}});
 			pluginID = valuesToSave.size()-1;
 		}
-		
+		boolean valueSet = false;
+		for (int i = 1; i < valuesToSave.get(pluginID).size(); i++) {
+			if (valuesToSave.get(pluginID).get(i).startsWith(varname + " = ")) {
+				valuesToSave.get(pluginID).set(i, varname + " = \"" + value + "\"");
+				valueSet = true;
+			}
+		}
+		if (!valueSet) {
+			valuesToSave.get(pluginID).add(varname + " = \"" + value + "\"");
+		}
 	}
 	
 	public void SaveToFile() throws IOException {
 		FileWriter fw = new FileWriter(path);
 		BufferedWriter bw = new BufferedWriter(fw);
-		
+		for (int p = 0; p < valuesToSave.size(); p++) {
+			bw.write("\r\n["  +valuesToSave.get(p).get(0) + "]\r\n");
+			for (int i = 1; i < valuesToSave.get(p).size(); i++) {
+				bw.write(valuesToSave.get(p).get(i) + "\r\n");
+			}
+		}
 		bw.close();
 		fw.close();
-		valuesToSave.clear();
 	}
 }
