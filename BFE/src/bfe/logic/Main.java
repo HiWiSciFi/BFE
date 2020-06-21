06ffb5e9193952dffa450727fd1ba7817aabddb4
HiWiSciFi <max.j.hager@gmail.com>
2020-06-21 18:13:08 +0200

    Version 1.1 built

--- BFE/src/bfe/logic/Main.java
+++ BFE/src/bfe/logic/Main.java
@@ -28,7 +28,7 @@ import bfe.gui.Preferences;
 
 public class Main {
 	
-	public static final String VERSION = "1.1 INDEV";
+	public static final String VERSION = "1.1";
 	public static final String NOTHING_SELECTED = "8e3242f8-23f5-4c28-b25b-ab802725d71a";
 	public static final char BREAKPOINT_CHARACTER = '!';
 	public static final String EMPTY_STRING = "";
