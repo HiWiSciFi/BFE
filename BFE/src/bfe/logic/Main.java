1e7d7c64fbd1ae892a72586c1df6bddc00b1aa90
HiWiSciFi <max.j.hager@gmail.com>
2020-11-01 16:53:45 +0100

    Fixed version naming issue

--- BFE/src/bfe/logic/Main.java
+++ BFE/src/bfe/logic/Main.java
@@ -29,7 +29,7 @@ import bfe.io.FileSystem;
 
 public class Main {
 	
-	public static final String VERSION = "1.3";
+	public static final String VERSION = "1.3 INDEV";
 	public static final String NOTHING_SELECTED = "8e3242f8-23f5-4c28-b25b-ab802725d71a";
 	public static final char BREAKPOINT_CHARACTER = '!';
 	public static final String EMPTY_STRING = "";
